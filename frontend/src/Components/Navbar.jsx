6853071581c17a3b8f909fe9348c8cf9f47fcd5d
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-16 08:23:25 +0530

    not final navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -13,17 +13,39 @@ const Navbar = () => {
   ]
   return (
     <div className="bg-inherit flex w-full p-5  justify-between md:justify-center items-center gap-4 md:gap-6">
-      <Link to="/" className="text-3xl font-medium">
+      <Link to="/" className=" text-3xl font-medium md:hidden">
         Zephyr
-      </Link>     
-          
-      <Menu
-            onClick={() => setIsLoggedIn(true)}
-            className="w-6 h-6  text-white hover:text-white md:hidden"
-          />
-       
+      </Link>
 
-     
+      <div className="hidden md:flex gap-5 rounded-full px-5 py-2 border border-gray-800 bg-gray-900">
+        <Link
+          to="/"
+          className="hover:backdrop-blur-sm hover:bg-white/20 font-medium md:rounded-full md:text-lg md:px-4 md:py-1 "
+        >
+          Zephyr
+        </Link>
+        <div className=" ">
+          <ul className="flex gap-5 ">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="px-4 py-2  rounded-full cursor-pointer hover:backdrop-blur-sm hover:bg-white/20 "
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+          </ul>
+        </div>
+      </div>
+
+      <Menu
+        onClick={() => setIsLoggedIn(true)}
+        className="w-6 h-6  text-white hover:text-white md:hidden"
+      />
     </div>
   );
 };
